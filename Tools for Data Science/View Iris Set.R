9ad3e0c58f7bff78a654ecd98a514fa405a2e05c
ameilij <ariel.meilij@gmail.com>
2022-10-01 17:18:31 -0500

    ADD: first folder

---
+++ Tools for Data Science/View Iris Set.R
@@ -0,0 +1,9 @@
+library(datasets)
+data(iris)
+View(iris)
+
+# Now let's find how many different species there are present in the 
+# data set. Type the following command into the editor window and click 
+# the run.
+unique(iris$Species)
+
